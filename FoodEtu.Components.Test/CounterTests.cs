6e7952068777d586df4cbb63f5c09ae2e8049ec3
Alexis C. GRIDEL <contact@agdl.dev>
2021-11-10 22:06:44 +0100

    feat: first component test

---
+++ FoodEtu.Components.Test/CounterTests.cs
@@ -0,0 +1,18 @@
+﻿using Bunit;
+using FoodEtu.Server.Pages;
+using Xunit;
+
+namespace FoodEtu.Components.Test;
+public class CounterTests
+{
+    [Fact]
+    public void CounterShouldIncrementWhenClicked()
+    {
+        using var ctx = new TestContext();
+        var cut = ctx.RenderComponent<Counter>();
+
+        cut.Find("button").Click();
+
+        cut.Find("p").MarkupMatches("<p role=\"status\">Current Alexandre count: 1</p>");
+    }
+}
