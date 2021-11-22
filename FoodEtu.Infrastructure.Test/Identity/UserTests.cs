acf9b58fe86a4e8b307ea758d0bebc9423d60aeb
Alexis C. GRIDEL <contact@agdl.dev>
2021-11-22 11:46:11 +0100

    feat: identity users units test init

---
+++ FoodEtu.Infrastructure.Test/Identity/UserTests.cs
@@ -0,0 +1,14 @@
+﻿using Moq;
+using Xunit;
+
+namespace FoodEtu.Infrastructure.Test.Identity;
+
+public class UserTests
+{
+    [Fact]
+    public void Register_User_Should_Insert_In_Users_Table()
+    {
+        var databaseContextMock = new Mock<FoodEtuDbContext>();
+    }
+}
+
